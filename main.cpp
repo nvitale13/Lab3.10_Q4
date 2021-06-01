1329c12fabfb4483f0fb4da95589c4669da1dfdc
nvitale13 <>
2021-06-01 01:27:48 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Modified version of original C program "
+            << " (by Kernighan & Ritchie)\n";
+
+  std::cout << "Hello World!";
+
+  return 0;
+}
